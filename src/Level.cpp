533111ed1b53c3d630567b2a1cb93716403f68f2
the2nake <home.vn2007@gmail.com>
2022-04-04 20:57:19 +0800

    Hotfix

--- src/Level.cpp
+++ src/Level.cpp
@@ -81,7 +81,7 @@ void Level::loadLevel(std::string playerName, std::string saveName, std::string 
             SDL_Rect dst = stringToSDLRect(tileEl.attribute("outRect").as_string());
             tempTile.texture = TextureManager::loadTexture(tileEl.attribute("src").as_string(), &src, &dst); // this should point to the same texture pointed to in TextureManager::loadedTextures
                                                                                                              // if there are duplcicates
-            tempTile.movementCost = tileEl.attribute("movementCost").as_int(-1);
+            tempTile.movementCost = tileEl.attribute("movementCost").as_int(1);
             tempTile.naturalTexture = tileEl.attribute("naturalTexture").as_bool();
             Level::tileDataLookup.insert_or_assign(tileEl.attribute("name").as_string(), tempTile);
         }
